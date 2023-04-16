c2abb9d738ff1c57dd73d1cc05eb54c60a50e760
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-17 01:41:49 +0300

    adding the auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { databaseProvider } from "./core/database/database.providers";
 import { CredentialsModule } from './modules/credentials/credentials.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ManagersModule } from './modules/managers/managers.module';
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), databaseProvider, CredentialsModule],
+	imports: [ConfigModule.forRoot({ isGlobal: true }), databaseProvider, CredentialsModule, AuthModule, ManagersModule],
 })
 export class AppModule {}
