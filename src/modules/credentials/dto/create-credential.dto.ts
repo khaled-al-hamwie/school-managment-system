526caeeefa7e830a85ed96f4bba823ac41ca930b
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 01:52:35 +0300

    adding create credentials dto and testing it

--- src/modules/credentials/dto/create-credential.dto.ts
+++ src/modules/credentials/dto/create-credential.dto.ts
@@ -1,1 +1,16 @@
-export class CreateCredentialDto {}
+import { IsEmail, Max, NotContains } from "class-validator";
+import NameValidator from "src/core/validators/name.validator";
+import { CredentialAttributes } from "../interfaces/credential.interface";
+
+export class CreateCredentialDto {
+	@IsEmail()
+	email: CredentialAttributes["email"];
+
+	@NotContains(" ", { message: "password should not contain a space" })
+	@NameValidator(20, 40)
+	password: CredentialAttributes["password"];
+
+	@NotContains(" ", { message: "user_name should not contain a space" })
+	@NameValidator(3, 45)
+	user_name: CredentialAttributes["user_name"];
+}
