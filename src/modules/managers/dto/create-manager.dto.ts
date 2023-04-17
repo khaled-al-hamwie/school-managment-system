5117e641280433bb01269f989c31deb80fd6a4a1
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 02:53:20 +0300

    adding the create manager dto and it 's test

--- src/modules/managers/dto/create-manager.dto.ts
+++ src/modules/managers/dto/create-manager.dto.ts
@@ -1,1 +1,25 @@
-export class CreateManagerDto {}
+import { IsNumber, IsPhoneNumber, IsPositive, Max } from "class-validator";
+import NameValidator from "src/core/validators/name.validator";
+import { CreateCredentialDto } from "src/modules/credentials/dto/create-credential.dto";
+import { ManagerAttributes } from "../interfaces/manager.interface";
+
+export class CreateManagerDto extends CreateCredentialDto {
+	@NameValidator(5, 45)
+	first_name: ManagerAttributes["first_name"];
+
+	@NameValidator(5, 45)
+	middle_name: ManagerAttributes["middle_name"];
+
+	@NameValidator(5, 45)
+	last_name: ManagerAttributes["last_name"];
+
+	@IsPhoneNumber("SY")
+	phone_number: ManagerAttributes["phone_number"];
+
+	@NameValidator(5, 45)
+	location: ManagerAttributes["location"];
+
+	@IsPositive()
+	@IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+	salary: ManagerAttributes["salary"];
+}
---
+++ src/modules/managers/tests/create-manager.dto.spec.ts
@@ -0,0 +1,103 @@
+import { plainToInstance } from "class-transformer";
+import { ValidationError, validate } from "class-validator";
+import { CreateManagerDto } from "../dto/create-manager.dto";
+
+let body: CreateManagerDto;
+
+describe("create manager dto", () => {
+	beforeEach(() => {
+		body = {
+			first_name: "test f",
+			middle_name: "test f",
+			last_name: "test f",
+			phone_number: "+963 944233644",
+			location: "damascus ,syria",
+			salary: 100,
+
+			email: "test@gmail.com",
+			password: "passwordpasswordpasswordpassword",
+			user_name: "usserName",
+		};
+	});
+	it("should pass", async () => {
+		const ofImportDto = plainToInstance(CreateManagerDto, body);
+		const erros: ValidationError[] = await validate(ofImportDto);
+		expect(erros.length).toBe(0);
+	});
+	describe("sallary", () => {
+		it("should not be null", async () => {
+			delete body["salary"];
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("salary");
+			expect(erros[0].constraints).toEqual({
+				isNumber:
+					"salary must be a number conforming to the specified constraints",
+				isPositive: "salary must be a positive number",
+			});
+		});
+		it("should be poositive", async () => {
+			body["salary"] = -12;
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("salary");
+			expect(erros[0].constraints).toEqual({
+				isPositive: "salary must be a positive number",
+			});
+		});
+		it("should be a number", async () => {
+			body["salary"] = NaN;
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("salary");
+			expect(erros[0].constraints).toEqual({
+				isNumber:
+					"salary must be a number conforming to the specified constraints",
+				isPositive: "salary must be a positive number",
+			});
+		});
+		it("should be less than 2-digit number", async () => {
+			body["salary"] = 1.213;
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("salary");
+			expect(erros[0].constraints).toEqual({
+				isNumber:
+					"salary must be a number conforming to the specified constraints",
+			});
+		});
+	});
+	describe("first_name,middle_name,last_name,location", () => {
+		it("should not allow no first_name", async () => {
+			delete body["first_name"];
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("first_name");
+			expect(erros[0].constraints).toEqual({
+				isString: "first_name must be a string",
+				isLength:
+					"first_name must be longer than or equal to 5 characters",
+			});
+		});
+		it("should not allow small first_name", async () => {
+			body["first_name"] = "1f      ";
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("first_name");
+			expect(erros[0].constraints).toEqual({
+				isLength:
+					"first_name must be longer than or equal to 5 characters",
+			});
+		});
+		it("should not allow large first_name", async () => {
+			body["first_name"] = "1f      ".repeat(44);
+			const ofImportDto = plainToInstance(CreateManagerDto, body);
+			const erros: ValidationError[] = await validate(ofImportDto);
+			expect(erros[0].property).toEqual("first_name");
+			expect(erros[0].constraints).toEqual({
+				isLength:
+					"first_name must be shorter than or equal to 45 characters",
+			});
+		});
+	});
+});
