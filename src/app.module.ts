0b92de24d91b25241d87ad2d126d97b7e1659852
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 01:12:34 +0300

    removing credentails module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { databaseProvider } from "./core/database/database.providers";
-import { CredentialsModule } from './modules/credentials/credentials.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ManagersModule } from './modules/managers/managers.module';
+import { AuthModule } from "./modules/auth/auth.module";
+import { ManagersModule } from "./modules/managers/managers.module";
 
 @Module({
-	imports: [ConfigModule.forRoot({ isGlobal: true }), databaseProvider, CredentialsModule, AuthModule, ManagersModule],
+	imports: [
+		ConfigModule.forRoot({ isGlobal: true }),
+		databaseProvider,
+		AuthModule,
+		ManagersModule,
+	],
 })
 export class AppModule {}
