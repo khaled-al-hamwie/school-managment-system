64ba786fc3900008a98965f646a1708fcfe556cd
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 01:53:04 +0300

    adding validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { HttpStatus, ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			stopAtFirstError: true,
+			whitelist: true,
+			errorHttpStatusCode: HttpStatus.FORBIDDEN,
+		})
+	);
+	await app.listen(4000);
 }
 bootstrap();
