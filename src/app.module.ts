99f018fb1e506af3f9787e58eb71823c35b55575
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 23:09:35 +0300

    adding a caching system for the get routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
+import { CacheInterceptor, CacheModule } from "@nestjs/cache-manager";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
 import { databaseProvider } from "./core/database/database.providers";
 import { ManagersModule } from "./modules/managers/managers.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
+		CacheModule.register({ ttl: 60, max: 1000, isGlobal: true }),
 		databaseProvider,
 		ManagersModule,
+	],
+	providers: [
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: CacheInterceptor,
+		},
 	],
 })
 export class AppModule {}
