fb969b718213fe2dfd6620bc2af455f74540faeb
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 17:16:02 +0300

    remove unused controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { databaseProvider } from "./core/database/database.providers";
-import { AuthModule } from "./modules/auth/auth.module";
 import { ManagersModule } from "./modules/managers/managers.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		databaseProvider,
-		AuthModule,
 		ManagersModule,
 	],
 })
--- src/modules/credentials/credentials.module.ts
+++ src/modules/credentials/credentials.module.ts
@@ -1,12 +1,10 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
-import { CredentialsController } from "./credentials.controller";
 import { CredentialsService } from "./credentials.service";
 import { Credential } from "./entities/credential.entity";
 
 @Module({
 	imports: [SequelizeModule.forFeature([Credential])],
-	controllers: [CredentialsController],
 	providers: [CredentialsService],
 	exports: [CredentialsService],
 })
