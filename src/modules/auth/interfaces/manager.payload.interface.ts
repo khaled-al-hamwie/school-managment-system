701e1ce0afd4792ddb688b2408157a7fad86f399
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-18 17:45:08 +0300

    adding manager payload

---
+++ src/modules/auth/interfaces/manager.payload.interface.ts
@@ -0,0 +1,8 @@
+import { CredentialAttributes } from "src/modules/credentials/interfaces/credential.interface";
+import { ManagerAttributes } from "src/modules/managers/interfaces/manager.interface";
+
+export default interface ManagerPayload {
+	credentail_id: CredentialAttributes["credential_id"];
+	user_name: CredentialAttributes["user_name"];
+	manager_id: ManagerAttributes["manager_id"];
+}
