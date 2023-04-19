712c2bfa4edbf7086d2cf7c9f1b259d8ac6059e6
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-19 03:12:08 +0300

    adding create teacher dto

--- src/modules/teachers/dto/create-teacher.dto.ts
+++ src/modules/teachers/dto/create-teacher.dto.ts
@@ -1,1 +1,44 @@
-export class CreateTeacherDto {}
+import { Transform, TransformFnParams } from "class-transformer";
+import {
+	IsDate,
+	IsNumber,
+	IsOptional,
+	IsPhoneNumber,
+	IsPositive,
+} from "class-validator";
+import tolowerCaseTransform from "src/core/transformers/tolowercase.transform";
+import NameValidator from "src/core/validators/name.validator";
+import { CreateManagerDto } from "src/modules/managers/dto/create-manager.dto";
+import { TeacherAttributes } from "../interfaces/teacher.interface";
+
+export class CreateTeacherDto extends CreateManagerDto {
+	@NameValidator(3, 16)
+	first_name: TeacherAttributes["first_name"];
+
+	@NameValidator(3, 16)
+	middle_name: TeacherAttributes["middle_name"];
+
+	@NameValidator(3, 16)
+	last_name: TeacherAttributes["last_name"];
+
+	@IsDate()
+	birth_day: TeacherAttributes["birth_day"];
+
+	@Transform(tolowerCaseTransform)
+	@NameValidator(1, 2)
+	gender: TeacherAttributes["gender"];
+
+	@IsOptional()
+	@NameValidator(2, 10)
+	nationality?: TeacherAttributes["nationality"];
+
+	@IsPhoneNumber("SY")
+	phone_number: TeacherAttributes["phone_number"];
+
+	@NameValidator(5, 45)
+	location: TeacherAttributes["location"];
+
+	@IsPositive()
+	@IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+	salary: TeacherAttributes["salary"];
+}
