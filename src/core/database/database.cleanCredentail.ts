d8418f02b71ac4743365aa9bfe09215a6e458388
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-27 00:15:52 +0300

    updating cleanCredentail cleaning student

--- src/core/database/database.cleanCredentail.ts
+++ src/core/database/database.cleanCredentail.ts
@@ -1,9 +1,11 @@
 import { Credential } from "src/modules/credentials/entities/credential.entity";
 import Manager from "src/modules/managers/entities/manager.entity";
+import Student from "src/modules/students/entities/student.entity";
 import Teacher from "src/modules/teachers/entities/teacher.entity";
 
 export default async function cleanCredential() {
 	await Manager.destroy({ where: {} });
 	await Teacher.destroy({ where: {} });
+	await Student.destroy({ where: {} });
 	await Credential.destroy({ where: {} });
 }
