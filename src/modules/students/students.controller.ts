83994d64d234ed8093a2c22417fd1196529ed688
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-27 21:54:18 +0300

    update student controller, adding show profile update and find one

--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -1,19 +1,23 @@
 import {
 	Body,
 	Controller,
-	Delete,
 	Get,
 	HttpCode,
 	HttpStatus,
+	NotFoundException,
 	Param,
+	ParseIntPipe,
 	Patch,
 	Post,
 	UseGuards,
 } from "@nestjs/common";
+import { User } from "src/core/decorators/user.decorator";
 import ManagerGuard from "src/core/guards/manager.guard";
+import StudentGuard from "src/core/guards/student.guard";
 import { CreateAuthDto } from "../auth/dto/create-auth.dto";
 import { CreateStudentDto } from "./dto/create-student.dto";
 import { UpdateStudentDto } from "./dto/update-student.dto";
+import { StudentAttributes } from "./interfaces/student.interface";
 import { StudentsService } from "./students.service";
 
 @Controller("students")
@@ -32,21 +36,33 @@ export class StudentsController {
 		return this.studentsService.login(body);
 	}
 
+	@UseGuards(ManagerGuard)
 	@Get()
 	findAll() {
 		return this.studentsService.findAll();
 	}
 
+	@UseGuards(StudentGuard)
+	@Get("profile")
+	showProfile(
+		@User("student_id") student_id: StudentAttributes["student_id"]
+	) {
+		return this.studentsService.findOne({ student_id });
+	}
+
+	@UseGuards(ManagerGuard)
 	@Get(":id")
-	findOne(@Param("id") id: string) {
-		return `${id} not implemented yet`;
+	async findOne(@Param("id", ParseIntPipe) student_id: string) {
+		const student = await this.studentsService.findOne({ student_id });
+		if (!student) throw new NotFoundException("student dosen't exists");
+		return student;
 	}
 
 	@Patch(":id")
 	update(
-		@Param("id") id: string,
+		@Param("id", ParseIntPipe) student_id: string,
 		@Body() updateStudentDto: UpdateStudentDto
 	) {
-		return this.studentsService.update(+id, updateStudentDto);
+		return this.studentsService.update(+student_id, updateStudentDto);
 	}
 }
