279d8be1da9ee78c2cd9fbf76dee5bd774cd8861
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-27 18:39:00 +0300

    update student controller add the login and findOne

--- src/modules/students/students.controller.ts
+++ src/modules/students/students.controller.ts
@@ -3,12 +3,15 @@ import {
 	Controller,
 	Delete,
 	Get,
+	HttpCode,
+	HttpStatus,
 	Param,
 	Patch,
 	Post,
 	UseGuards,
 } from "@nestjs/common";
 import ManagerGuard from "src/core/guards/manager.guard";
+import { CreateAuthDto } from "../auth/dto/create-auth.dto";
 import { CreateStudentDto } from "./dto/create-student.dto";
 import { UpdateStudentDto } from "./dto/update-student.dto";
 import { StudentsService } from "./students.service";
@@ -23,6 +26,12 @@ export class StudentsController {
 		return this.studentsService.create(createStudentDto);
 	}
 
+	@Post("/login")
+	@HttpCode(HttpStatus.OK)
+	login(@Body() body: CreateAuthDto) {
+		return this.studentsService.login(body);
+	}
+
 	@Get()
 	findAll() {
 		return this.studentsService.findAll();
@@ -30,7 +39,7 @@ export class StudentsController {
 
 	@Get(":id")
 	findOne(@Param("id") id: string) {
-		return this.studentsService.findOne(+id);
+		return `${id} not implemented yet`;
 	}
 
 	@Patch(":id")
@@ -39,10 +48,5 @@ export class StudentsController {
 		@Body() updateStudentDto: UpdateStudentDto
 	) {
 		return this.studentsService.update(+id, updateStudentDto);
-	}
-
-	@Delete(":id")
-	remove(@Param("id") id: string) {
-		return this.studentsService.remove(+id);
 	}
 }
