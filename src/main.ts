ea42e412842b68539afd83e38c56c3a8db0c29ca
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-04-29 02:20:19 +0300

    adding compression

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from "@nestjs/core";
+import * as compression from "compression";
 import { AppModule } from "./app.module";
 import { MainValidationPipe } from "./core/pipes/MainValidation.pipe";
 import swaggerSetup from "./core/swagger/swagger.setup";
@@ -6,6 +7,7 @@ import swaggerSetup from "./core/swagger/swagger.setup";
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(MainValidationPipe);
+	app.use(compression());
 	swaggerSetup(app);
 	await app.listen(4000);
 }
