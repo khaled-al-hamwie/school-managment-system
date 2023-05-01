03044910ebd16d177281b54f6cd2cc7fcd9af303
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-01 21:15:12 +0300

    adding the name property swagger

--- src/core/validators/name.validator.ts
+++ src/core/validators/name.validator.ts
@@ -1,10 +1,12 @@
 import { applyDecorators } from "@nestjs/common";
+import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
 import { IsString, Length } from "class-validator";
 import trimTransform from "../transformers/trim.transformer";
 
 export default function NameValidator(min: number, max: number) {
 	return applyDecorators(
+		ApiProperty({ minimum: min, maximum: max }),
 		IsString(),
 		Transform(trimTransform),
 		Length(min, max)
