ba935c5bea58c1186346e00ab2b8e318a45467af
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-05 22:29:48 +0300

    running format

--- src/repl.ts
+++ src/repl.ts
@@ -1,9 +1,9 @@
-import { repl } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { repl } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
     const replServer = await repl(AppModule);
-    replServer.setupHistory('.nestjs_repl_history', (err) => {
+    replServer.setupHistory(".nestjs_repl_history", (err) => {
         if (err) {
             console.error(err);
         }
