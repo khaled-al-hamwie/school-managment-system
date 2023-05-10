cdfae4325aa13ad649a82fd8b1edfdf6c9054029
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-10 21:41:27 +0300

    fixing the subject remove test

--- src/modules/subjects/tests/subjects.service.spec.ts
+++ src/modules/subjects/tests/subjects.service.spec.ts
@@ -1,15 +1,11 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "src/app.module";
-import { ClassesModule } from "src/modules/classes/classes.module";
-import { ClassesService } from "src/modules/classes/classes.service";
-import { bodySample } from "src/modules/classes/tests/sample";
 import { setTimeout } from "timers/promises";
 import { CreateSubjectDto } from "../dto/create-subject.dto";
 import { SubjectsService } from "../subjects.service";
 
 describe("subject Service", () => {
     let service: SubjectsService;
-    let classesService: ClassesService;
     let body: CreateSubjectDto;
     beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
@@ -17,10 +13,9 @@ describe("subject Service", () => {
         }).compile();
 
         service = module.get<SubjectsService>(SubjectsService);
-        classesService = module.get<ClassesService>(ClassesService);
-        classesService.create(bodySample);
+
         body = {
-            class_id: (await classesService.findAll(""))[0].class_id,
+            class_id: 1,
             name: "math",
             semester: 1,
         };
@@ -30,7 +25,7 @@ describe("subject Service", () => {
         expect(service).toBeDefined();
     });
 
-    it("should create a class", async () => {
+    it("should create a subject", async () => {
         const output = await service.create(body);
         expect(output).toBe("done");
     });
@@ -47,7 +42,8 @@ describe("subject Service", () => {
         expect(output).toBe("done");
     });
     it("should remove", async () => {
-        const subject = (await service.findOne({ name: body.name })).subject_id;
+        const subject = (await service.findOne({ name: "fjds;lak;jfdsalk" }))
+            .subject_id;
         const output = service.remove(subject);
         expect(output).toBe("done");
     });
