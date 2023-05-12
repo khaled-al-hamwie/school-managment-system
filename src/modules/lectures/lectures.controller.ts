e379ad633433d1e0a8683905351e347d9edd4892
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-12 20:34:25 +0300

    adding lecure module

--- src/modules/lectures/lectures.controller.ts
+++ src/modules/lectures/lectures.controller.ts
@@ -1,16 +1,21 @@
 import {
-    Controller,
-    Get,
-    Post,
     Body,
-    Patch,
-    Param,
+    Controller,
     Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
 } from "@nestjs/common";
-import { LecturesService } from "./lectures.service";
 import { CreateLectureDto } from "./dto/create-lecture.dto";
 import { UpdateLectureDto } from "./dto/update-lecture.dto";
+import { LecturesService } from "./lectures.service";
 
+// see a schedual for a certain room public(in the room/roomid/scedual)
+// see the lecture for a specific teacher only teachers (teacher/scedual) how has that id or manager
+// add a lecture for a class manager
+// remove a lecture for a class manager
+// update a lecture for a class manager
 @Controller("lectures")
 export class LecturesController {
     constructor(private readonly lecturesService: LecturesService) {}
@@ -23,11 +28,6 @@ export class LecturesController {
     @Get()
     findAll() {
         return this.lecturesService.findAll();
-    }
-
-    @Get(":id")
-    findOne(@Param("id") id: string) {
-        return this.lecturesService.findOne(+id);
     }
 
     @Patch(":id")
--- src/modules/lectures/lectures.module.ts
+++ src/modules/lectures/lectures.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
-import { LecturesService } from "./lectures.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { Lecture } from "./entities/lecture.entity";
 import { LecturesController } from "./lectures.controller";
+import { LecturesService } from "./lectures.service";
 
 @Module({
+    imports: [SequelizeModule.forFeature([Lecture])],
     controllers: [LecturesController],
     providers: [LecturesService],
 })
