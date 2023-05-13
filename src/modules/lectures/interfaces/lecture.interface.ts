11387505f618dd32871c72c736838e3a01ae5b64
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-13 17:58:41 +0300

    updating the dayType

--- src/modules/lectures/interfaces/lecture.interface.ts
+++ src/modules/lectures/interfaces/lecture.interface.ts
@@ -1,5 +1,5 @@
 import { Optional } from "sequelize";
-import { DayTye } from "src/core/common/types/day.type";
+import { DayType } from "src/core/common/types/day.type";
 import { RoomAttributes } from "src/modules/rooms/interfaces/room.interface";
 import { TeachAttributes } from "src/modules/teaches/interfaces/teach.interface";
 
@@ -7,7 +7,7 @@ export interface LectureAttributes {
     lecture_id?: number;
     room_id: RoomAttributes["room_id"];
     teach_id?: TeachAttributes["teach_id"];
-    day: DayTye;
+    day: DayType;
     start_time: string;
     is_rest?: boolean;
 }
