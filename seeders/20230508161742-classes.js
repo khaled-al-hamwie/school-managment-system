a613e5b476e6a495474dcee5d9cd9296bf761adf
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-14 18:23:56 +0300

    update the class seeder

--- seeders/20230508161742-classes.js
+++ seeders/20230508161742-classes.js
@@ -7,23 +7,14 @@ module.exports = {
       {
         class_id: 1,
         name: "Class 1",
-        number_of_lectures: 10,
-        lecture_length: 60,
-        rest_length: 10,
       },
       {
         class_id: 2,
         name: "Class 2",
-        number_of_lectures: 12,
-        lecture_length: 45,
-        rest_length: 15,
       },
       {
         class_id: 3,
         name: "Class 3",
-        number_of_lectures: 8,
-        lecture_length: 90,
-        rest_length: 20,
       },
     ]);
   },
