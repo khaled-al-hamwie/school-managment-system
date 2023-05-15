12a796994820a370890005b5f0f64bc434f6827a
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-16 00:30:38 +0300

    schedule adding the findOne schedule

--- src/modules/schedules/schedules.service.ts
+++ src/modules/schedules/schedules.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
+import {
+    FindAttributeOptions,
+    FindOptions,
+    Includeable,
+    WhereOptions,
+} from "sequelize";
+import { Lecture } from "../lectures/entities/lecture.entity";
 import { RoomsService } from "../rooms/rooms.service";
+import { ScheduleDay } from "../schedule_days/entities/schedule_day.entity";
 import { ScheduleDaysService } from "../schedule_days/schedule_days.service";
 import { CreateScheduleDto } from "./dto/create-schedule.dto";
 import { UpdateScheduleDto } from "./dto/update-schedule.dto";
 import { Schedule } from "./entities/schedule.entity";
-import { ScheduleAttributes } from "./interfaces/schedule.interface";
+import {
+    ScheduleAttributes,
+    ScheduleCreationAttributes,
+} from "./interfaces/schedule.interface";
 
 @Injectable()
 export class SchedulesService {
@@ -46,12 +57,36 @@ export class SchedulesService {
         });
     }
 
-    findAll() {
-        return `This action returns all schedules`;
+    findAll(
+        where: WhereOptions<ScheduleAttributes>,
+        attributes?: (keyof ScheduleCreationAttributes)[]
+    ) {
+        return this.ScheduleEntity.findAll({
+            where,
+            attributes,
+        });
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} schedule`;
+    findOne(
+        where: WhereOptions<ScheduleAttributes>,
+        attributes?: (keyof ScheduleCreationAttributes)[],
+        include?: Includeable | Includeable[]
+    ) {
+        return this.ScheduleEntity.findOne({
+            where,
+            attributes,
+            include,
+            order: [
+                // [Schedule.associations.schedule_days, "day", "DESC"],
+                [{ model: ScheduleDay, as: "schedule_days" }, "day", "ASC"],
+                [
+                    { model: ScheduleDay, as: "schedule_days" },
+                    { model: Lecture, as: "lectures" },
+                    "start_time",
+                    "ASC",
+                ],
+            ],
+        });
     }
 
     update(id: number, updateScheduleDto: UpdateScheduleDto) {
@@ -62,3 +97,7 @@ export class SchedulesService {
         return `This action removes a #${id} schedule`;
     }
 }
+// fri sat sun mon tue
+// fri mon sat sun tue
+
+// sat sun mon tue fri
