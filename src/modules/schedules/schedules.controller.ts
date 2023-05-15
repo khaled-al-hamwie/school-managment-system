e3e8a04751e6c6b825bdb75940706776e214f95c
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-15 17:39:00 +0300

    schedule adding the create schedule

--- src/modules/schedules/schedules.controller.ts
+++ src/modules/schedules/schedules.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { SchedulesService } from './schedules.service';
-import { CreateScheduleDto } from './dto/create-schedule.dto';
-import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post,
+    UseGuards,
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import ManagerGuard from "src/core/common/guards/manager.guard";
+import { SCHEDULE_TAG, WEB_TAG } from "src/core/swagger/constants/swagger.tags";
+import { CreateScheduleDto } from "./dto/create-schedule.dto";
+import { UpdateScheduleDto } from "./dto/update-schedule.dto";
+import { ScheduleAttributes } from "./interfaces/schedule.interface";
+import { SchedulesService } from "./schedules.service";
 
-@Controller('schedules')
+@ApiTags(SCHEDULE_TAG)
+@Controller("rooms/:room_id/schedules")
 export class SchedulesController {
-  constructor(private readonly schedulesService: SchedulesService) {}
+    constructor(private readonly schedulesService: SchedulesService) {}
 
-  @Post()
-  create(@Body() createScheduleDto: CreateScheduleDto) {
-    return this.schedulesService.create(createScheduleDto);
-  }
+    @ApiTags(WEB_TAG)
+    @ApiBearerAuth("Authorization")
+    @UseGuards(ManagerGuard)
+    @Post()
+    create(
+        @Param("room_id", ParseIntPipe) room_id: ScheduleAttributes["room_id"],
+        @Body() createScheduleDto: CreateScheduleDto
+    ) {
+        return this.schedulesService.create(room_id, createScheduleDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.schedulesService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.schedulesService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.schedulesService.findOne(+id);
-  }
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        return this.schedulesService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateScheduleDto: UpdateScheduleDto) {
-    return this.schedulesService.update(+id, updateScheduleDto);
-  }
+    @Patch(":id")
+    update(
+        @Param("id") id: string,
+        @Body() updateScheduleDto: UpdateScheduleDto
+    ) {
+        return this.schedulesService.update(+id, updateScheduleDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.schedulesService.remove(+id);
-  }
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.schedulesService.remove(+id);
+    }
 }
--- src/modules/schedules/schedules.module.ts
+++ src/modules/schedules/schedules.module.ts
@@ -1,11 +1,17 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
+import { RoomsModule } from "../rooms/rooms.module";
+import { ScheduleDaysModule } from "../schedule_days/schedule_days.module";
 import { Schedule } from "./entities/schedule.entity";
 import { SchedulesController } from "./schedules.controller";
 import { SchedulesService } from "./schedules.service";
 
 @Module({
-    imports: [SequelizeModule.forFeature([Schedule])],
+    imports: [
+        SequelizeModule.forFeature([Schedule]),
+        RoomsModule,
+        ScheduleDaysModule,
+    ],
     controllers: [SchedulesController],
     providers: [SchedulesService],
 })
