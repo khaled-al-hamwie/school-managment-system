1abe16d2f9a08b4417f556cffd1377b5494ba89d
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-15 13:46:07 +0300

    schedule day adding the create schedule day

--- src/modules/schedule_days/dto/create-schedule_day.dto.ts
+++ src/modules/schedule_days/dto/create-schedule_day.dto.ts
@@ -1,1 +1,10 @@
-export class CreateScheduleDayDto {}
+import { CreateScheduleDto } from "src/modules/schedules/dto/create-schedule.dto";
+import { ScheduleDayAttributes } from "../interfaces/schedule_day.interface";
+
+export class CreateScheduleDayDto {
+    schedule_id: ScheduleDayAttributes["schedule_id"];
+    days: CreateScheduleDto["days"];
+    start_time: ScheduleDayAttributes["start_time"];
+    lecture_number: ScheduleDayAttributes["lecture_number"];
+    rests: CreateScheduleDto["rests"];
+}
