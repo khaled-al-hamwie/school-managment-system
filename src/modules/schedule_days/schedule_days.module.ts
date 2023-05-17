398d748901c4ff3918f3f0aac02438fe4a273773
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-17 22:16:44 +0300

    schedule day ,adding the lecture number

--- src/modules/schedule_days/schedule_days.module.ts
+++ src/modules/schedule_days/schedule_days.module.ts
@@ -2,12 +2,10 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { LecturesModule } from "../lectures/lectures.module";
 import { ScheduleDay } from "./entities/schedule_day.entity";
-import { ScheduleDaysController } from "./schedule_days.controller";
 import { ScheduleDaysService } from "./schedule_days.service";
 
 @Module({
     imports: [SequelizeModule.forFeature([ScheduleDay]), LecturesModule],
-    controllers: [ScheduleDaysController],
     providers: [ScheduleDaysService],
     exports: [ScheduleDaysService],
 })
