a54fc4f990984765f3d9874609ffda16a3af042b
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-17 18:35:45 +0300

    update room service ,updating the update room dto

--- src/modules/rooms/dto/update-room.dto.ts
+++ src/modules/rooms/dto/update-room.dto.ts
@@ -2,5 +2,5 @@ import { PartialType, PickType } from "@nestjs/swagger";
 import { CreateRoomDto } from "./create-room.dto";
 
 export class UpdateRoomDto extends PartialType(
-    PickType(CreateRoomDto, ["name", "student_count"])
+    PickType(CreateRoomDto, ["name", "student_count", "class_id"])
 ) {}
