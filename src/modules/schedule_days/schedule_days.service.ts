6002c2477eaaa392ba1cf6951a87296baf416adc
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-18 22:29:04 +0300

    schedule day ,adding the update schedule day

--- src/modules/schedule_days/schedule_days.service.ts
+++ src/modules/schedule_days/schedule_days.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/sequelize";
-import { addTimes } from "src/core/common/transformers/addTimes.transform";
+import { FindAttributeOptions, WhereOptions } from "sequelize";
 import { LecturesService } from "../lectures/lectures.service";
+import {
+    DayDto,
+    UpdateScheduleDto,
+} from "../schedules/dto/update-schedule.dto";
 import { CreateScheduleDayDto } from "./dto/create-schedule_day.dto";
 import { UpdateScheduleDayDto } from "./dto/update-schedule_day.dto";
 import { ScheduleDay } from "./entities/schedule_day.entity";
+import { ScheduleDayAttributes } from "./interfaces/schedule_day.interface";
 
 @Injectable()
 export class ScheduleDaysService {
@@ -33,16 +38,65 @@ export class ScheduleDaysService {
         }
     }
 
-    findAll() {
-        return `This action returns all scheduleDays`;
+    findAll(
+        where: WhereOptions<ScheduleDayAttributes>,
+        attributes?: FindAttributeOptions,
+        limit?: number
+    ) {
+        return this.ScheduleDayEntity.findAll({
+            where,
+            limit,
+            attributes,
+        });
     }
 
     findOne(id: number) {
         return `This action returns a #${id} scheduleDay`;
     }
+    async update(body: UpdateScheduleDayDto) {
+        const scheduleDays = await this.checkDay(body);
 
-    update(id: number, updateScheduleDayDto: UpdateScheduleDayDto) {
-        return `This action updates a #${id} scheduleDay`;
+        for (let i = 0; i < body.days.length; i++) {
+            let schedule_day = scheduleDays.find(
+                (schedule_day_object) =>
+                    schedule_day_object.day == body.days[i].day
+            );
+            await this.lecturesService.updateLectures({
+                schedule_day_id: schedule_day.schedule_day_id,
+                lectures: body.days[i].lectures,
+            });
+        }
+    }
+
+    private async checkDay(body: UpdateScheduleDayDto) {
+        const scheduleDays = await this.findAll(
+            { schedule_id: body.schedule_id },
+            ["day", "schedule_day_id", "lecture_number"],
+            body.days_count
+        );
+        const days = scheduleDays.map((day) => day.day);
+        const error = [];
+        body.days.forEach((day_object) => {
+            if (this.scheduleNotIncludeDay(days, day_object))
+                error.push(
+                    `the day ${day_object.day} is not allowed provied a day from ${days}`
+                );
+            day_object.lectures.forEach((lecture) => {
+                if (lecture.lecture_number > scheduleDays[0].lecture_number)
+                    error.push(
+                        `the lecture_number ${lecture.lecture_number} on the day ${day_object.day} is not allowed please provide a value <=${scheduleDays[0].lecture_number}`
+                    );
+            });
+        });
+        if (error.length >= 1) throw new ForbiddenException(error);
+        return scheduleDays;
+    }
+
+    private scheduleNotIncludeDay(
+        days: ("sat" | "sun" | "mon" | "tue" | "wed" | "thu" | "fri")[],
+        day_object: DayDto
+    ) {
+        return !days.includes(day_object.day);
     }
 
     remove(id: number) {
