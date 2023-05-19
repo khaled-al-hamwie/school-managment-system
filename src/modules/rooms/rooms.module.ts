fcaa0af85e82b9d88bb6a84136d309a69c66cb4d
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-19 23:47:27 +0300

    update room ,update the create room to add students

--- src/modules/rooms/rooms.module.ts
+++ src/modules/rooms/rooms.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { ClassesModule } from "../classes/classes.module";
+import { RecordsModule } from "../records/records.module";
 import { SchedulesModule } from "../schedules/schedules.module";
+import { StudentsModule } from "../students/students.module";
 import { Room } from "./entities/room.entity";
 import { RoomsController } from "./rooms.controller";
 import { RoomsService } from "./rooms.service";
@@ -11,6 +13,8 @@ import { RoomsService } from "./rooms.service";
         SequelizeModule.forFeature([Room]),
         ClassesModule,
         SchedulesModule,
+        RecordsModule,
+        StudentsModule,
     ],
     controllers: [RoomsController],
     providers: [RoomsService],
