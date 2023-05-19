e46ce818d9f3482c39f1501987e527ef0c0f1252
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-19 23:08:07 +0300

    update record service ,adding the add records findOne and recordNotExists and testing them

--- src/modules/records/records.controller.ts
+++ src/modules/records/records.controller.ts
@@ -1,34 +1,17 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { RecordsService } from './records.service';
-import { CreateRecordDto } from './dto/create-record.dto';
-import { UpdateRecordDto } from './dto/update-record.dto';
+import { Controller, Get, Param } from "@nestjs/common";
+import { RecordsService } from "./records.service";
 
-@Controller('records')
+@Controller("records")
 export class RecordsController {
-  constructor(private readonly recordsService: RecordsService) {}
+    constructor(private readonly recordsService: RecordsService) {}
 
-  @Post()
-  create(@Body() createRecordDto: CreateRecordDto) {
-    return this.recordsService.create(createRecordDto);
-  }
+    @Get()
+    findAll() {
+        return this.recordsService.findAll();
+    }
 
-  @Get()
-  findAll() {
-    return this.recordsService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.recordsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRecordDto: UpdateRecordDto) {
-    return this.recordsService.update(+id, updateRecordDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.recordsService.remove(+id);
-  }
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        // return this.recordsService.findOne(+id);
+    }
 }
---
+++ src/modules/records/tests/records.service.spec.ts
@@ -0,0 +1,82 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppModule } from "src/app.module";
+import Student from "src/modules/students/entities/student.entity";
+import { Record } from "../entities/record.entity";
+import { RecordsService } from "../records.service";
+
+describe("RecordsService", () => {
+    let service: RecordsService;
+
+    beforeAll(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        service = module.get<RecordsService>(RecordsService);
+    });
+
+    it("should be defined", () => {
+        expect(service).toBeDefined();
+    });
+    it("findOne", async () => {
+        await Record.create({
+            class_id: 1,
+            record_id: 1,
+            student_id: 1,
+            year: "2020/2021",
+        });
+        expect(
+            await service.findOne({
+                class_id: 1,
+                record_id: 1,
+                student_id: 1,
+                year: "2020/2021",
+            })
+        ).not.toBeNull();
+        expect(
+            await service.findOne({
+                class_id: -1,
+                record_id: 1,
+                student_id: 1,
+                year: "2020/2021",
+            })
+        ).toBeNull();
+    });
+
+    it("record not exist", async () => {
+        expect(await service.recordNotExist(-1, -1, "2020/2021")).toBeTruthy();
+        expect(await service.recordNotExist(1, 1, "2020/2021")).toBeFalsy();
+    });
+
+    it("craete", async () => {
+        await Student.create({
+            student_id: 2,
+            credential_id: 2,
+            first_name: "khaled",
+            last_name: "Teacher",
+            birth_day: "2000-01-20",
+            gender: "m",
+            nationality: "UK",
+            phone_number: "0963351256",
+            location: "some where in UK",
+            father_name: "fjdsalk",
+            mother_name: "lfkdsa;",
+        });
+        await service.create(2, [1, 2, 2]);
+        expect(
+            await service.findOne({ class_id: 2, student_id: 1 })
+        ).toBeNull();
+        expect(
+            await service.findOne({ class_id: 2, student_id: 2 })
+        ).not.toBeNull();
+        expect(
+            (await Record.findAll({ where: { class_id: 2, student_id: 2 } }))
+                .length
+        ).toBe(1);
+    });
+
+    afterAll(async () => {
+        await Student.destroy({ where: {} });
+        await Record.destroy({ where: {} });
+    });
+});
