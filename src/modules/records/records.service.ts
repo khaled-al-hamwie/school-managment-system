fa9fbeb1781537afcc92229f32ab3c2539266b27
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-19 23:08:07 +0300

    update record service ,adding the add records findOne and recordNotExists and testing them

--- src/modules/records/records.service.ts
+++ src/modules/records/records.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRecordDto } from './dto/create-record.dto';
-import { UpdateRecordDto } from './dto/update-record.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { WhereOptions } from "sequelize";
+import { StudentsService } from "../students/students.service";
+import { Record } from "./entities/record.entity";
+import { RecordAttributes } from "./interfaces/record.interface";
 
 @Injectable()
 export class RecordsService {
-  create(createRecordDto: CreateRecordDto) {
-    return 'This action adds a new record';
-  }
+    constructor(
+        @InjectModel(Record) private readonly RecordEntity: typeof Record,
+        private readonly studentsService: StudentsService
+    ) {}
+    async create(
+        class_id: RecordAttributes["class_id"],
+        student_ids: RecordAttributes["student_id"][]
+    ) {
+        const year = `${new Date().getFullYear()}/${
+            new Date().getFullYear() + 1
+        }`;
+        for (let i = 0; i < student_ids.length; i++) {
+            const student_id = student_ids[i];
+            const student = await this.studentsService.findOne({
+                student_id,
+            });
+            if (
+                student &&
+                (await this.recordNotExist(class_id, student_id, year))
+            )
+                await this.RecordEntity.create({ class_id, student_id, year });
+        }
+    }
 
-  findAll() {
-    return `This action returns all records`;
-  }
+    findAll() {
+        return `This action returns all records`;
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} record`;
-  }
+    findOne(options: WhereOptions<RecordAttributes>) {
+        return this.RecordEntity.findOne({
+            where: options,
+            limit: 1,
+        });
+    }
 
-  update(id: number, updateRecordDto: UpdateRecordDto) {
-    return `This action updates a #${id} record`;
-  }
+    async recordNotExist(
+        class_id: RecordAttributes["class_id"],
+        student_id: RecordAttributes["student_id"],
+        year: RecordAttributes["year"]
+    ) {
+        return (await this.findOne({ class_id, student_id, year })) == null;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} record`;
-  }
+    update() {
+        return `This action updates a  record`;
+    }
+
+    remove(id: number) {
+        return `This action removes a #${id} record`;
+    }
 }
