daddccdcf0372cbf9b2b95dd628ac26a6a28cdc1
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-19 16:03:17 +0300

    update the schedule find one,adding the full name of the teacher and end time

--- src/modules/schedules/constants/index.ts
+++ src/modules/schedules/constants/index.ts
@@ -1,13 +1,16 @@
-import { Includeable, Order } from "sequelize";
+import { Includeable, Order, Sequelize } from "sequelize";
 import { Lecture } from "src/modules/lectures/entities/lecture.entity";
 import { ScheduleDay } from "src/modules/schedule_days/entities/schedule_day.entity";
+import { Subject } from "src/modules/subjects/entities/subject.entity";
+import Teacher from "src/modules/teachers/entities/teacher.entity";
+import { Teach } from "src/modules/teaches/entities/teach.entity";
 import { ScheduleCreationAttributes } from "../interfaces/schedule.interface";
 
 export const scheduleAttributes: (keyof ScheduleCreationAttributes)[] = [
     "schedule_id",
     "room_id",
     "title",
-    "is_current",
+    "lecture_length",
     "days_count",
     "created_at",
     "updated_at",
@@ -19,7 +22,64 @@ export const scheduleInclude: Includeable | Includeable[] = [
         include: [
             {
                 model: Lecture,
-                attributes: { exclude: ["schedule_day_id"] },
+                attributes: {
+                    exclude: ["schedule_day_id"],
+                    include: [
+                        [
+                            Sequelize.fn(
+                                "ADDTIME",
+                                Sequelize.col(
+                                    "schedule_days->lectures.start_time"
+                                ),
+                                Sequelize.fn(
+                                    "IF",
+                                    Sequelize.col("is_rest"),
+                                    "00:15:00",
+                                    "00:45:00"
+                                )
+                            ),
+                            "end_time",
+                        ],
+                    ],
+                },
+
+                include: [
+                    {
+                        model: Teach,
+                        required: false,
+                        attributes: { exclude: ["teach_id"] },
+                        include: [
+                            {
+                                model: Teacher,
+                                attributes: {
+                                    exclude: [
+                                        "teacher_id",
+                                        "credential_id",
+                                        "birth_day",
+                                        "nationality",
+                                        "phone_number",
+                                        "location",
+                                        "salary",
+                                    ],
+                                    include: [
+                                        [
+                                            Sequelize.literal(
+                                                'CONCAT(first_name, " ", last_name)'
+                                            ),
+                                            "full_name",
+                                        ],
+                                    ],
+                                },
+                            },
+                            {
+                                model: Subject,
+                                attributes: {
+                                    exclude: ["semester", "subject_id"],
+                                },
+                            },
+                        ],
+                    },
+                ],
             },
         ],
     },
