22c34ba1333a715aed3724ff60de383e2820e6b5
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-20 19:41:02 +0300

    adding classes and rooms seeders

--- seeders/20230508161742-classes.js
+++ seeders/20230508161742-classes.js
@@ -1,25 +1,26 @@
 "use strict";
 
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("Classes", [
-      {
-        class_id: 1,
-        name: "Class 1",
-      },
-      {
-        class_id: 2,
-        name: "Class 2",
-      },
-      {
-        class_id: 3,
-        name: "Class 3",
-      },
-    ]);
+    const classes = await JSON.parse(
+      readFileSync(resolve("seeders", "json", "class.json")),
+      { encoding: "utf8" }
+    );
+
+    const rooms = await JSON.parse(
+      readFileSync(resolve("seeders", "json", "room.json")),
+      { encoding: "utf8" }
+    );
+    await queryInterface.bulkInsert("Classes", classes);
+    await queryInterface.bulkInsert("Rooms", rooms);
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("Classes", null, {});
+    await queryInterface.bulkDelete("Rooms", null, {});
   },
 };
