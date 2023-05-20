2825a38dce1fcf6ad78cc9f51e78b0e200759008
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-20 13:53:00 +0300

    schedule controller ,adding the findStudentSchedule

--- src/modules/schedules/schedules.module.ts
+++ src/modules/schedules/schedules.module.ts
@@ -1,12 +1,17 @@
 import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { ScheduleDaysModule } from "../schedule_days/schedule_days.module";
+import { StudentsModule } from "../students/students.module";
 import { Schedule } from "./entities/schedule.entity";
 import { SchedulesController } from "./schedules.controller";
 import { SchedulesService } from "./schedules.service";
 
 @Module({
-    imports: [SequelizeModule.forFeature([Schedule]), ScheduleDaysModule],
+    imports: [
+        SequelizeModule.forFeature([Schedule]),
+        ScheduleDaysModule,
+        StudentsModule,
+    ],
     controllers: [SchedulesController],
     providers: [SchedulesService],
     exports: [SchedulesService],
