6ed586de72f18c5b4bae298379032f71549fc0ae
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-23 15:02:36 +0300

    update the port

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from "@nestjs/core";
+import { NestExpressApplication } from "@nestjs/platform-express";
 import * as compression from "compression";
+import { join } from "path";
 import { AppModule } from "./app.module";
 import { MainValidationPipe } from "./core/common/pipes/MainValidation.pipe";
 import swaggerSetup from "./core/swagger/swagger.setup";
-import { NestExpressApplication } from "@nestjs/platform-express";
-import { join } from "path";
 declare const module: any;
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,7 +12,7 @@ async function bootstrap() {
     app.useGlobalPipes(MainValidationPipe);
     app.use(compression());
     swaggerSetup(app);
-    await app.listen(4000);
+    await app.listen(process.env.PORT || 4000);
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(() => app.close());
