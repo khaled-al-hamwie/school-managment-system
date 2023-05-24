2b7b8c6d6b4e4f99bd23d396d82d605dbba9bcdc
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-24 22:01:46 +0300

    subject seeder ,adding the subjects to database

--- seeders/20230508173729-subjects.js
+++ seeders/20230508173729-subjects.js
@@ -1,9 +1,25 @@
 "use strict";
+const { readFileSync } = require("fs");
+const { resolve } = require("path");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("Subjects", []);
+    const subjects = await JSON.parse(
+      readFileSync(resolve("seeders", "json", "subject.json")),
+      { encoding: "utf8" }
+    );
+    for (let i = 0; i < subjects.length; i++) {
+      const subject = subjects[i];
+      await queryInterface.bulkInsert("Subjects", [
+        {
+          subject_id: i + 1,
+          class_id: subject.class_id,
+          name: subject.name,
+          semester: subject.semester,
+        },
+      ]);
+    }
   },
 
   async down(queryInterface, Sequelize) {
