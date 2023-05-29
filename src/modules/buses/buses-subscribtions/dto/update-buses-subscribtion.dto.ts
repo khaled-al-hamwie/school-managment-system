00359763852b179cce39f6f646f3a3541cdec480
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-29 17:32:40 +0300

    update bus subscribtion ,remove unused files

--- src/modules/buses/buses-subscribtions/dto/update-buses-subscribtion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateBusesSubscribtionDto } from './create-buses-subscribtion.dto';
-
-export class UpdateBusesSubscribtionDto extends PartialType(CreateBusesSubscribtionDto) {}
--- src/modules/buses/buses.module.ts
+++ src/modules/buses/buses.module.ts
@@ -2,7 +2,6 @@ import { Module } from "@nestjs/common";
 import { SequelizeModule } from "@nestjs/sequelize";
 import { StudentsModule } from "../students/students.module";
 import { BusesSubscribtionsController } from "./buses-subscribtions/buses-subscribtions.controller";
-import { BusesSubscribtionsService } from "./buses-subscribtions/buses-subscribtions.service";
 import { BusesController } from "./buses.controller";
 import { BusesService } from "./buses.service";
 import { Bus } from "./entities/bus.entity";
@@ -10,6 +9,6 @@ import { Bus } from "./entities/bus.entity";
 @Module({
     imports: [SequelizeModule.forFeature([Bus]), StudentsModule],
     controllers: [BusesController, BusesSubscribtionsController],
-    providers: [BusesService, BusesSubscribtionsService],
+    providers: [BusesService],
 })
 export class BusesModule {}
