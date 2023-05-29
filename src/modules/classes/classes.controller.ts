0023e163c80f35c822085b776407fba49e6c80d6
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-30 00:08:05 +0300

    update class controller get class ,returning the subject of a class

--- src/modules/classes/classes.controller.ts
+++ src/modules/classes/classes.controller.ts
@@ -14,6 +14,7 @@ import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import ManagerGuard from "src/core/common/guards/manager.guard";
 import { CLASS_TAG, WEB_TAG } from "src/core/swagger/constants/swagger.tags";
 import { Room } from "../rooms/entities/room.entity";
+import { Subject } from "../subjects/entities/subject.entity";
 import { ClassesService } from "./classes.service";
 import { CreateClassDto } from "./dto/create-class.dto";
 import { UpdateClassDto } from "./dto/update-class.dto";
@@ -46,7 +47,7 @@ export class ClassesController {
     ) {
         const myClass = await this.classesService.findOne({
             where: { class_id },
-            include: [{ model: Room }],
+            include: [{ model: Subject }, { model: Room }],
         });
         if (!myClass) throw new NotFoundException("class doesn't exist");
         return myClass;
