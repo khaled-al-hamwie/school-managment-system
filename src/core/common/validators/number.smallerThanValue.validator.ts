d386a3dd6b83860e0af9185a27dc02b0d51786f3
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-29 13:51:41 +0300

    adding the number smaller than value validator

---
+++ src/core/common/validators/number.smallerThanValue.validator.ts
@@ -0,0 +1,25 @@
+import {
+    ValidationArguments,
+    ValidatorConstraint,
+    ValidatorConstraintInterface,
+} from "class-validator";
+
+@ValidatorConstraint({ name: "", async: false })
+export class NumberSmallerThanValue implements ValidatorConstraintInterface {
+    validate(
+        value: number,
+        validationArguments?: ValidationArguments
+    ): boolean | Promise<boolean> {
+        console.info(validationArguments);
+        if (
+            value &&
+            value >
+                validationArguments.object[validationArguments.constraints[0]]
+        )
+            return false;
+        return true;
+    }
+    defaultMessage?(validationArguments?: ValidationArguments): string {
+        return `the ${validationArguments.property} of value ${validationArguments.value} can't be bigger than the ${validationArguments.constraints[0]}`;
+    }
+}
