b819d3f1da094913935515938f3daca865093b35
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-29 14:44:57 +0300

    update bus ,adding the create and find All

--- src/modules/buses/buses.controller.ts
+++ src/modules/buses/buses.controller.ts
@@ -1,34 +1,57 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { BusesService } from './buses.service';
-import { CreateBusDto } from './dto/create-bus.dto';
-import { UpdateBusDto } from './dto/update-bus.dto';
-
-@Controller('buses')
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+    UseGuards,
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { WhereOptions } from "sequelize";
+import ManagerGuard from "src/core/common/guards/manager.guard";
+import { BUS_TAG, WEB_TAG } from "src/core/swagger/constants/swagger.tags";
+import whereWrapperTransform from "../../core/common/transformers/whereWrapper.transform";
+import { BusesService } from "./buses.service";
+import { CreateBusDto } from "./dto/create-bus.dto";
+import { UpdateBusDto } from "./dto/update-bus.dto";
+import { BusAttributes } from "./interfaces/bus.interface";
+@ApiTags(BUS_TAG, WEB_TAG)
+@Controller("buses")
 export class BusesController {
-  constructor(private readonly busesService: BusesService) {}
+    constructor(private readonly busesService: BusesService) {}
 
-  @Post()
-  create(@Body() createBusDto: CreateBusDto) {
-    return this.busesService.create(createBusDto);
-  }
+    @ApiBearerAuth("Authorization")
+    @UseGuards(ManagerGuard)
+    @Post()
+    create(@Body() createBusDto: CreateBusDto) {
+        return this.busesService.create(createBusDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.busesService.findAll();
-  }
+    @ApiBearerAuth("Authorization")
+    @UseGuards(ManagerGuard)
+    @Get()
+    findAll(@Query("name") name: string | null) {
+        let where: WhereOptions<BusAttributes> = name
+            ? whereWrapperTransform({ name })
+            : {};
+        return this.busesService.findAll({ where });
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.busesService.findOne(+id);
-  }
+    @Get(":id")
+    findOne(@Param("id") id: string) {
+        return this.busesService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBusDto: UpdateBusDto) {
-    return this.busesService.update(+id, updateBusDto);
-  }
+    @Patch(":id")
+    update(@Param("id") id: string, @Body() updateBusDto: UpdateBusDto) {
+        return this.busesService.update(+id, updateBusDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.busesService.remove(+id);
-  }
+    @Delete(":id")
+    remove(@Param("id") id: string) {
+        return this.busesService.remove(+id);
+    }
 }
--- src/modules/buses/buses.service.ts
+++ src/modules/buses/buses.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
-import { CreateBusDto } from './dto/create-bus.dto';
-import { UpdateBusDto } from './dto/update-bus.dto';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { FindOptions } from "sequelize";
+import { CreateBusDto } from "./dto/create-bus.dto";
+import { UpdateBusDto } from "./dto/update-bus.dto";
+import { Bus } from "./entities/bus.entity";
+import { BusAttributes } from "./interfaces/bus.interface";
 
 @Injectable()
 export class BusesService {
-  create(createBusDto: CreateBusDto) {
-    return 'This action adds a new bus';
-  }
+    constructor(@InjectModel(Bus) private readonly BusEntity: typeof Bus) {}
+    async create(createBusDto: CreateBusDto) {
+        this.BusEntity.create({
+            ...createBusDto,
+            arrival_time: "07:30",
+            pick_up_time: "12:30",
+        });
+        return "done";
+    }
 
-  findAll() {
-    return `This action returns all buses`;
-  }
+    findAll(options: FindOptions<BusAttributes>) {
+        return this.BusEntity.findAll(options);
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} bus`;
-  }
+    findOne(id: number) {
+        return `This action returns a #${id} bus`;
+    }
 
-  update(id: number, updateBusDto: UpdateBusDto) {
-    return `This action updates a #${id} bus`;
-  }
+    update(id: number, updateBusDto: UpdateBusDto) {
+        return `This action updates a #${id} bus`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} bus`;
-  }
+    remove(id: number) {
+        return `This action removes a #${id} bus`;
+    }
 }
