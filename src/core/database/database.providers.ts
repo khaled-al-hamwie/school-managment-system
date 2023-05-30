bf16caa408ea8ba39621f763f4191b867a668136
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-05-30 13:02:29 +0300

    fixing the deploy

--- src/core/database/database.providers.ts
+++ src/core/database/database.providers.ts
@@ -19,7 +19,7 @@ switch (process.env.NODE_ENV) {
         config = databaseConfig.development;
 }
 
-createDatabase(config.username, config.password, config.database, config.host);
+// createDatabase(config.username, config.password, config.database, config.host);
 export const databaseProvider: DynamicModule = SequelizeModule.forRoot({
     ...config,
     autoLoadModels: true,
