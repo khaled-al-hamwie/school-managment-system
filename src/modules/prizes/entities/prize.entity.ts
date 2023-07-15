5f3e3071fb338a74550ea5f12e2cf326a3ca9258
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-07-15 23:25:19 +0300

    adding the prise enttiy and interface

--- src/modules/prizes/entities/prize.entity.ts
+++ src/modules/prizes/entities/prize.entity.ts
@@ -1,1 +1,55 @@
-export class Prize {}
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import {
+    PriseAttributes,
+    PriseCreationAttributes,
+} from "../interfaces/prise.interface";
+
+@Table
+export class Prise
+    extends Model<PriseAttributes, PriseCreationAttributes>
+    implements PriseCreationAttributes
+{
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+    })
+    prise_id!: PriseAttributes["prise_id"];
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: false,
+    })
+    name!: PriseAttributes["name"];
+
+    @Column({
+        type: DataType.STRING(500),
+        allowNull: true,
+    })
+    details?: PriseAttributes["details"];
+
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        allowNull: false,
+    })
+    price!: PriseAttributes["price"];
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: DataType.NOW,
+    })
+    created_at!: PriseAttributes["created_at"];
+
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        allowNull: false,
+    })
+    count!: PriseAttributes["count"];
+
+    @Column({
+        type: DataType.STRING(255),
+        allowNull: true,
+    })
+    picture?: PriseAttributes["picture"];
+}
