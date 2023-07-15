6e8ff336a25d9f9859cecf4e21e35bf0c8f3ddd5
khaledUserName <khaledalhamwiedevelopmacccount@gmail.com>
2023-07-15 23:30:18 +0300

    adding the transaction entity

--- src/modules/transactions/entities/transaction.entity.ts
+++ src/modules/transactions/entities/transaction.entity.ts
@@ -1,1 +1,49 @@
-export class Transaction {}
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+import {
+    TransactionAttributes,
+    TransactionCreationAttributes,
+} from "../interfaces/transaction.interface";
+
+@Table
+export class Transaction
+    extends Model<TransactionAttributes, TransactionCreationAttributes>
+    implements TransactionCreationAttributes
+{
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+    })
+    transaction_id?: TransactionAttributes["transaction_id"];
+
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        allowNull: false,
+    })
+    teacher_id!: TransactionAttributes["teacher_id"];
+
+    @Column({
+        type: DataType.INTEGER.UNSIGNED,
+        allowNull: false,
+    })
+    student_id!: TransactionAttributes["student_id"];
+
+    @Column({
+        type: DataType.FLOAT.UNSIGNED,
+        allowNull: false,
+    })
+    value!: TransactionAttributes["value"];
+
+    @Column({
+        type: DataType.STRING(500),
+        allowNull: false,
+    })
+    reason!: TransactionAttributes["reason"];
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: false,
+        defaultValue: DataType.NOW,
+    })
+    created_at?: TransactionAttributes["created_at"];
+}
